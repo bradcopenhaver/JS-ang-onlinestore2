13aeaf9d0cf170deadad89ebfdd76166d3016a6a
Brad Copenhaver and Frankie Gavin <brad@>
2017-01-25 15:55:07 -0800

    add edit album component to admin page

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -13,7 +13,8 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 export class MarketplaceComponent implements OnInit {
 
   albums: FirebaseListObservable<any[]>;
-
+  currentRoute: string = this.router.url;
+   
   constructor (private router: Router, private albumService: AlbumService){}
 
   ngOnInit(){
